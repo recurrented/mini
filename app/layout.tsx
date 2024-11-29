02f0a0f6f985a630dfdcdc553d1fcdbc557a6a5e
Austin Chen <akrolsmir@gmail.com>
2024-11-28 22:47:35 -0700

    Add og metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,29 @@
-import './globals.css'
+import "./globals.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "minifest • Dec 14, 2024",
+  description:
+    "A cozy one-day festival in Berkeley celebrating prediction markets, blogging, economics, and more",
+  openGraph: {
+    title: "minifest • Dec 14, 2024",
+    description:
+      "A cozy one-day festival in Berkeley celebrating prediction markets, blogging, economics, and more",
+    url: "https://minifest.is",
+    siteName: "minifest",
+    locale: "en_US",
+    type: "website",
+  },
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
