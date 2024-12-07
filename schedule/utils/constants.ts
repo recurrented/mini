009eab87ffe47f27f19630cf93727d517b462edb
rachelweinberg12 <rachel.weinberg12@gmail.com>
2024-12-06 19:53:52 -0800

    Fix nav item type error

--- schedule/utils/constants.ts
+++ schedule/utils/constants.ts
@@ -9,5 +9,5 @@ export const CONSTS = {
   DESCRIPTION:
     "We've booked out Lighthaven for a single day, Dec 14. We'll have unconference-style sessions, a smattering of events like charity poker, and ample time to hang out and chat. We're running this on a shoestring budget, so that everyone can come -- eg expect a homecooked dinner instead of catering. It'll be a cozy, lowkey, experimental version of the Manifest you know and love.",
   MULTIPLE_EVENTS: false,
-  NAV_ITEMS: [],
+  NAV_ITEMS: [] as NavItem[],
 };
