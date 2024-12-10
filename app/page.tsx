3fe659651b6945b192ee4b25813551bfe6a53de8
Austin Chen <akrolsmir@gmail.com>
2024-12-10 11:04:22 -0800

    Link charity poker

--- app/page.tsx
+++ app/page.tsx
@@ -103,11 +103,17 @@ export default async function Component() {
             <p className="mb-4">
               And so: minifest! We've booked out Lighthaven for a single day,
               Dec 14. We'll have unconference-style sessions, a smattering of
-              events like charity poker, and ample time to hang out and chat.
-              We're running this on a shoestring budget, so that everyone can
-              come -- eg expect a homecooked dinner instead of catering. It'll
-              be a cozy, lowkey, experimental version of the Manifest you know
-              and love.
+              events like{" "}
+              <a
+                href="https://lu.ma/paqqi50m"
+                className="underline underline-offset-2 font-semibold"
+              >
+                charity poker
+              </a>
+              , and ample time to hang out and chat. We're running this on a
+              shoestring budget, so that everyone can come -- eg expect a
+              homecooked dinner instead of catering. It'll be a cozy, lowkey,
+              experimental version of the Manifest you know and love.
             </p>
 
             <p className="italic">
